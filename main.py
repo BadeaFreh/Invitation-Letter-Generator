263ac66f1edd33826b361c0893a010d8488b22f4
BadeaFreh <badeafreh@gmail.com>
2023-09-18 18:47:22 +0300

    update names

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+PLACEHOLDER = '[name]'
 
 with open('./Input/Letters/starting_letter.txt') as text: 
     starting_letter = text.read()
@@ -6,6 +7,6 @@ with open('./Input/Names/invited_names.txt') as names:
     names_arr = names.read().split('\n')
 
 for name in names_arr:
-    cur_letter = starting_letter.replace('[name]', name)
+    cur_letter = starting_letter.replace(PLACEHOLDER, name)
     with open(f'./Output/ReadyToSend/letter_for_{name}.txt', mode='w') as new_letter:
         new_letter.write(cur_letter)
